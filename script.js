94500328270c8ff5484db74d67d95ba6fa8e0bbe
tomasabril909 <tomasabril350@gmail.com>
2024-11-11 03:00:46 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 document.getElementById('colorButton').addEventListener('click', function() {
-    // Obtiene el color actual del fondo
-    var currentColor = document.body.style.backgroundColor;
-
-    // Cambia entre los colores "negro", "rosa" y "blanco"
-    if (currentColor === 'black') {
-        document.body.style.backgroundColor = 'pink';
-    } else if (currentColor === 'pink') {
-        document.body.style.backgroundColor = 'white';
+    // Comprobar la clase actual del body y cambiarla
+    if (document.body.classList.contains('black-theme')) {
+        document.body.classList.remove('black-theme');
+        document.body.classList.add('pink-theme');
+    } else if (document.body.classList.contains('pink-theme')) {
+        document.body.classList.remove('pink-theme');
+        document.body.classList.add('white-theme');
     } else {
-        document.body.style.backgroundColor = 'black';
+        document.body.classList.remove('white-theme');
+        document.body.classList.add('black-theme');
     }
 });
